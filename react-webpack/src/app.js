617e9780bb90c0dbe2b586a74c7d65f8a4050b65
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-02 12:02:00 -0300

    convert js sintaxe to jsx sintaxe in .babelrc and prop source-map in webpack.config

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Title = React.createClass({
     render: function() {
-        return React.createElement('h1', null, 'My first component')
+        return <h1>My first component</h1>;
     }
 })
 
