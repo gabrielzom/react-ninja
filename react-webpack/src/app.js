e72f794f5df8fdc418d86cfea9f5b92d0da64284
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-06 10:53:33 -0300

    with use propTypes

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,33 +1,14 @@
 'use strict'
 import React, { Component } from 'react'
-import Timer from './timer.js'
+import Button from './button'
 class App extends Component {
-
-  constructor () {
-    console.log('constructor app')
-    super()
-    this.state = {
-      time: 0,
-      showTimer: true
-    }
-  }
-
-  componentWillMount () {
-    console.log('will mount app')
-  }
-
-  componentDidMount () {
-    console.log('did mount app')
-  }
-
   render () {
     console.log('render app')
     return (
       <div>
-        <Timer time={this.state.time} />
-        <button onClick={() => {
-          this.setState({ time: this.state.time + 1 })
-        }}>Change Props</button>
+        <Button handleClick={() => alert('Clicked')}>
+          Clique here
+        </Button>
       </div>
     )
   }
