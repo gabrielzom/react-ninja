c823e9cf9cdb80277b1400b8390eb7ba7150f0b0
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-06 11:06:25 -0300

    using forms

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,14 +1,26 @@
 'use strict'
 import React, { Component } from 'react'
-import Button from './button'
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: 'initial value 2'
+    }
+  }
   render () {
     console.log('render app')
     return (
       <div>
-        <Button handleClick={() => alert('Clicked')}>
-          Clique here
-        </Button>
+        <form>
+          <input type='text' value={this.state.value}
+            onChange={(e) => {
+              console.log(e)
+              this.setState({
+                value: e.target.value
+              })
+            }}
+          />
+        </form>
       </div>
     )
   }
