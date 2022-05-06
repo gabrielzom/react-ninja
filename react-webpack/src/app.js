e15df18854dc6bbed2787170addf981cdd498ccb
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-06 09:11:34 -0300

    learning lifecycle

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -5,13 +5,19 @@ import Square from './square.js'
 class App extends Component {
 
   constructor () {
+    console.log('constructor')
     super()
     this.state = {
       color: 'green'
     }
   }
 
+  componentWillMount () {
+    console.log('will mount')
+  }
+
   render () {
+    console.log('render')
     return (
       <div>
         <Square backgroundColor={this.state.color} />
